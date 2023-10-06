15c70e3cf4d9091adcc2205bc08db6ef1797d38a
azzeddinetiba <azzeddine.tiba@gmail.com>
2023-10-07 01:15:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-#pyth_pip_path=$(python -c "exec(\"import site\nprint(site.getsitepackages()[0])\")")
+pyth_pip_path=$(python -c "exec(\"import site\nprint(site.getsitepackages()[0])\")")
 
 
-#pip install KratosMultiphysics==9.4
-#pip install KratosLinearSolversApplication==9.4 > /dev/null 2>&1
-#pip install KratosFluidDynamicsApplication==9.4 > /dev/null 2>&1
-#pip install KratosStructuralMechanicsApplication==9.4 > /dev/null 2>&1
-#pip install KratosMeshMovingApplication==9.4 > /dev/null 2>&1
-#pip install KratosMappingApplication==9.4 > /dev/null 2>&1
-#pip install KratosConstitutiveLawsApplication==9.4 > /dev/null 2>&1
-#pip install KratosCoSimulationApplication==9.4 > /dev/null 2>&1
+pip install KratosMultiphysics==9.4
+pip install KratosLinearSolversApplication==9.4 > /dev/null 2>&1
+pip install KratosFluidDynamicsApplication==9.4 > /dev/null 2>&1
+pip install KratosStructuralMechanicsApplication==9.4 > /dev/null 2>&1
+pip install KratosMeshMovingApplication==9.4 > /dev/null 2>&1
+pip install KratosMappingApplication==9.4 > /dev/null 2>&1
+pip install KratosConstitutiveLawsApplication==9.4 > /dev/null 2>&1
+pip install KratosCoSimulationApplication==9.4 > /dev/null 2>&1
 
-#rm -r $pyth_pip_path/KratosMultiphysics/CoSimulationApplication/
-#cp -r FsiROM_Kratos_patch/CoSimulationApplication/ $pyth_pip_path/KratosMultiphysics/CoSimulationApplication/
-#cp FsiROM_Kratos_patch/fluid_solver.py $pyth_pip_path/KratosMultiphysics/FluidDynamicsApplication/
+rm -r $pyth_pip_path/KratosMultiphysics/CoSimulationApplication/
+cp -r FsiROM_Kratos_patch/CoSimulationApplication/ $pyth_pip_path/KratosMultiphysics/CoSimulationApplication/
+cp FsiROM_Kratos_patch/fluid_solver.py $pyth_pip_path/KratosMultiphysics/FluidDynamicsApplication/
 
 
-#git clone --single-branch --branch Workshop https://github.com/azzeddinetiba/ROM_AM.git > /dev/null 2>&1
-#pip install ROM_AM/.
-#pip install pyvista  > /dev/null 2>&1
+git clone --single-branch --branch Workshop https://github.com/azzeddinetiba/ROM_AM.git > /dev/null 2>&1
+pip install ROM_AM/.
+pip install pyvista  > /dev/null 2>&1
 
-#export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=1
 

361bdd2cd6f63c4de65e77f04f79c3af0cbe1220
Azzeddine TIBA <azzeddine.tiba@gmail.com>
2023-10-17 19:02:48 +0200

    Option for time prediction only

--- RomMainKratos.py
+++ RomMainKratos.py
@@ -2,16 +2,18 @@ import KratosMultiphysics as KM
 from KratosMultiphysics.CoSimulationApplication.co_Romsimulation_analysis import CoRomSimulationAnalysis
 from myModels import *
 
-
-"""
-For user-scripting it is intended that a new class is derived
-from CoSimulationAnalysis to do modifications
-Check also "kratos/python_scripts/analysis_stage.py" for available methods that can be overridden
-"""
+timepredictionOnly = False
 
 parameter_file_name = "ProjectParametersCoSim.json"
-with open(parameter_file_name,'r') as parameter_file:
+with open(parameter_file_name, 'r') as parameter_file:
     parameters = KM.Parameters(parameter_file.read())
 
-simulation = CoRomSimulationAnalysis(parameters,)
+if not timepredictionOnly:
+    simulation = CoRomSimulationAnalysis(parameters,)
+else:
+    disp_latentDim = .9999
+    force_latentDim = 24
+    simulation = CoRomSimulationAnalysis(parameters, inputReduc_model=MyForceReducer(force_latentDim),
+                                         regression_model=MyRegressor(),
+                                         outputReduc_model=MyDispReducer(disp_latentDim))
 simulation.Run()
